891ba81a6aff01a237477ec09833ec3230d0591e
Jochen Voss <voss@seehuhn.de>
2020-11-01 10:50:38 +0000

    updates

---
+++ doc.go
@@ -0,0 +1,5 @@
+// Package letsencrypt is the (empty) root of the module tree.
+//
+// seehuhn.de/go/letsencrypt/cert - helper package
+// seehuhn.de/go/letsencrypt/getcert - command line client
+package letsencrypt
